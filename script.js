15029946dc9d1d789b4d1eaaf51446ed7a6c0468
iannyip <ianyip@nullspace.com.sg>
2021-01-28 19:48:49 +0800

    arrangment, close button, and animation

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ document.body.appendChild(mainContainer);
 
 // Create each card and append
 const cardArray = [];
+const rowArray = [];
 for (let i = 0; i < 60; i +=1){
+  
   // Create each card
   cardArray[i] = document.createElement('section');
   cardArray[i].classList.add('card-style')
@@ -22,9 +24,15 @@ for (let i = 0; i < 60; i +=1){
 
   // Add img to each modal
   const popUpVerse = document.createElement('img');
-  popUpVerse.classList.add('verse-img-style')
+  popUpVerse.classList.add('verse-img-style');
   popUpVerse.src = `memory-cards/Memory cards-1-${ ('0'+ (i+1)).substr(-2)}.png`;
   modal.appendChild(popUpVerse);
+
+  // Add close button to each modal
+  const closeBtn = document.createElement('div');
+  closeBtn.classList.add('closeBtn');
+  modal.appendChild(closeBtn);
+  closeBtn.innerHTML = 'X';
 
   // Card card click functionality
   cardArray[i].addEventListener('click', () => {
@@ -34,7 +42,7 @@ for (let i = 0; i < 60; i +=1){
   })
   // When click in white space, remove the pop up img
   modal.addEventListener('click', (event) =>{
-    if (event.target === modal) {
+    if (event.target === modal || event.target === closeBtn) {
     console.log(event.target);
     modal.style.display = "none";
   }
